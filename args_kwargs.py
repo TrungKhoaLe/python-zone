ecbc88be93e5622060567ad5e1fa27a1590f9c3a
KL <khoaletrung@gmail.com>
2023-07-18 20:57:05 +0700

    Reviewing args and kwards and adding one case for printer 4

--- args_kwargs.py
+++ args_kwargs.py
@@ -65,23 +65,29 @@ if __name__ == '__main__':
         print('[INFO] Example 1')
         print(f"Result with three positional arguments {addition(3, 4, 5)}")
         print(f"Result with four positional arguments {addition(3, 4, 5, 6)}")
+
     elif args['example'] == '2':
         print('[INFO] Example 2')
         print(arg_printer(3, 4, 5, 8, 3))
+
     elif args['example'] == '3':
         print('[INFO] Example 3')
         print('[INFO] Key word arguments are always after the positional' +
               'arguments')
+
     elif args['example'] == '4':
         print('[INFO] Example 4')
         print(f'Result with option equals True: {addition_1(10, 11, 12)}')
         print(f'Result with option equals False: {addition_1(10, 11, 12, option=False)}')
+
     elif args['example'] == '5':
         print('[INFO] Example 5')
         print(arg_printer_1(4, 5, option=False, param1=2, param2=1))
+
     elif args['example'] == '6':
         print('[INFO] Example 6')
         print(arg_printer_2(5, 6, 7, 8, 9, 10, param1=2, param2=5))
+
     elif args['example'] == '7':
         print('[INFO] Example 7')
         print('[INFO] Packing and unpacking variables using *args')
@@ -89,7 +95,12 @@ if __name__ == '__main__':
         print(f'Passing a list without unpacking: {arg_printer_3([1, 2, 4])}')
         # it is like passing more than 1 variable to the function
         print(f'Passing an unpacked list: {arg_printer_3(*[1, 2, 4])}')
+
     else:
         print('[INFO] Example 8')
         print('[INFO] Packing and unpacking variables using **kwargs')
-        print(arg_printer_4(**{'guava': 20, 'orange': 10}))
+        try:
+            print(f"Passing a dictionary without unpacking: {arg_printer_4({'guava': 20, 'orange': 10})}")
+        except TypeError:
+            print('\033[93;1m[WARNING] The function expects key word arguments, but we passed in a positional argumet, hence the TypeError being raised!\033[0m')
+        print(f"Passing an unpacked dictionary: {arg_printer_4(**{'guava': 20, 'orange': 10})}")
