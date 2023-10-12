e7d6b723fc0e8e7123bc316d2d792a98d3d7ffe3
KL <khoaletrung@gmail.com>
2023-10-12 22:28:00 +0700

    Update kwargs

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,121 +1,13 @@
-import argparse
+# Example 1: unpacking keyword arguments
+def my_plotter(ax, data1, data2, param_dict):
+    out = ax.plot(data1, data2, **param_dict)
+    return out
 
-"""
-- When an asterisk (*) stays before a vairable name in a Python function, Python
-understands that the number of arguments here varies. Python forms a TUPLE from
-those arguments with the name defined after the asterisk and makes the variable
-available inside the function. This asterisk is also called the 'unpacking
-operator'.
-
-- **kwargs allows us to pass any number of keyword arguments to a function,
-  which is equivalent to passing any Python dictionaries to the function. 
-"""
+# call the function
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-ex', '--example', type=str, help='Choose an example to\
-run')
-args = vars(parser.parse_args())
-
-
-def addition(*args):
-    '''
-    *args allow a function to take any number of positional arguments
-    '''
-    result = 0
-    for i in args:
-        result += i
-    return result
-
-
-def addition_1(a, b, *args, option=True):
-    result = 0
-    if option:
-        for i in args:
-            result += i
-        return a + b + result
-    else:
-        return result
-
-
-def arg_printer(a, b, *args):
-    '''
-    *args can be used with named variables
-    '''
-    print(f'a: {a}')
-    print(f'b: {b}')
-    print(f'args: {args}')
-
-
-def arg_printer_1(a, b, option=True, **kwargs):
-    '''
-    **kwargs allow a function to take any number of keyword arguments
-    '''
-    print(a, b)
-    print(option)
-    print(kwargs)
-
-
-def arg_printer_2(a, b, *args, option=True, **kwargs):
-    print(a, b)
-    print(args)
-    print(option)
-    print(kwargs)
-
-
-def arg_printer_3(*args):
-    print(args)
-
-
-def arg_printer_4(**kwargs):
-    print(kwargs)
-
-
-if __name__ == '__main__':
-    if args['example'] == '1':
-        print('[INFO] Example 1')
-        print(f"Result with three positional arguments {addition(3, 4, 5)}")
-        print(f"Result with four positional arguments {addition(3, 4, 5, 6)}")
-
-    elif args['example'] == '2':
-        print('[INFO] Example 2')
-        print(arg_printer(3, 4, 5, 8, 3))
-
-    elif args['example'] == '3':
-        print('[INFO] Example 3')
-        print('[INFO] Key word arguments are always after the positional' +
-              'arguments')
-
-    elif args['example'] == '4':
-        print('[INFO] Example 4')
-        print(f'Result with option equals True: {addition_1(10, 11, 12)}')
-        print(f'Result with option equals False: {addition_1(10, 11, 12, option=False)}')
-
-    elif args['example'] == '5':
-        print('[INFO] Example 5')
-        print(arg_printer_1(4, 5, option=False, param1=2, param2=1))
-
-    elif args['example'] == '6':
-        print('[INFO] Example 6')
-        print(arg_printer_2(5, 6, 7, 8, 9, 10, param1=2, param2=5))
-
-    elif args['example'] == '7':
-        print('[INFO] Example 7')
-        print('[INFO] Packing and unpacking variables using *args')
-        # it is like passing 1 variable to the function
-        print(f'Passing a list without unpacking: {arg_printer_3([1, 2, 4])}')
-        # it is like passing more than 1 variable to the function
-        print(f'Passing an unpacked list: {arg_printer_3(*[1, 2, 4])}')
-
-    else:
-        print('[INFO] Example 8')
-        print('[INFO] Packing and unpacking variables using **kwargs')
-        try:
-            print("Passing a dictionary without unpacking: ")
-            arg_printer_4({"guava": 20, "orange": 10})
-        except TypeError:
-            print('\033[93;1m[WARNING] The function expects key word arguments, but we passed in a positional argumet, hence the TypeError being raised!\033[0m')
-        print("Passing an unpacked dictionary: ")
-        arg_printer_4(**{"guava": 20, "orange": 10})
-        print("Another way of passing arguments to the printer 4: ")
-        arg_printer_4(guava=20, orange=10)
+data1, data2 = np.random.randn(2, 100)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(5, 2.7))
+my_plotter(ax1, data1, data2, {'marker': 'x'})
